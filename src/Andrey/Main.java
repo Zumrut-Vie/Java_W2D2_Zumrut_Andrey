d4a3ce3ccf72890fd72593fdf94f6051c5857011
Andrey Aleksovski <aleksovski_andrey@hotmail.com>
2020-07-07 14:16:20 +0200

    finished A3

--- src/Andrey/Main.java
+++ src/Andrey/Main.java
@@ -2,7 +2,13 @@ package Andrey;
 public class Main {
 
     public static void main(String[] args) {
+        Bike bike1 = new Bike();
+        bike1.move(10);
+        bike1.move(10);
+        bike1.applyBreaks(5);
+        System.out.println(bike1.speed);
     }
+
 
 
     }
